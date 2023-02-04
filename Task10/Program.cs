6128f17150cbb91d600e0dc9e64c10e6986eed9d
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-04 22:53:32 +0300

    Программа для вывода второй цифры в трёхзначных числах

---
+++ Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа для вывода второй цифры в трёхзначных числах.
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 100;
+int c = b / 10;
+Console.WriteLine($"Вторая цифра здесь: {c}");
