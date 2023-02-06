3ed210e5433e900d4d9b237f6b5e0a11fa964da9
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-06 10:03:28 +0300

    Программа для вывода третьей цифры в ведённом числе или сообщении об её отсутствии

---
+++ Task13/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа для вывода третьей цифры в ведённом числе или сообщении об её отсутствии.
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a <= 99)
+Console.WriteLine($"В данном числе - третьей цифры нет...");
+else
+{while (a >= 1000)
+{a = a / 10;}
+int b = a % 10;
+Console.WriteLine($"В этом числе третьей цифрой будет: {b}");};
