a5d667498b83ad1f543c27e1a14a55fe3836e93d
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-09 11:39:30 +0300

    Программа, которая выдаёт таблицу кубов от 1 до Z

---
+++ Task23/Program.cs
@@ -0,0 +1,6 @@
+﻿// Программа, которая выдаёт таблицу кубов от 1 до Z.
+Console.Clear();
+Console.Write("Введите число: ");
+int Z = Convert.ToInt32(Console.ReadLine());
+for (int X = 1; X <= Z; X++)
+Console.Write($"{Math.Pow(X, 3)} ");
