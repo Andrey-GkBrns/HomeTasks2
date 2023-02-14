071c412160c0fcebe5b5a94027575fe1291e9704
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-14 15:33:43 +0300

    Программа для посдчёта суммы чисел введённого числа

---
+++ Task27/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа для посдчёта суммы чисел введённого числа.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int X = Convert.ToInt32(Console.ReadLine()), count = 0;
+while (X > 0)
+{
+count = count + X % 10;
+X = X / 10;
+}
+Console.WriteLine($"Сумма чисел равна: {count}");
