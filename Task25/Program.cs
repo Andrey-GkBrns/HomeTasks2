326e1afebe02688b7dcdeee65baa1cbdf8791c19
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-14 14:38:49 +0300

    Программа для поиска натуральной степени при вводе соответствующих значений

---
+++ Task25/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа для поиска натуральной степени при вводе соответствующих значений.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int Y = Convert.ToInt32(Console.ReadLine()), count = 1;
+for (int Z = 1; Z <= Y; Z++)
+count = count * X;
+Console.Write($"Результат: {count}");
