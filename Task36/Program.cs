3121484be30ef02498eb5a24da9c264035ddb7fc
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-16 18:46:06 +0300

    Программа для определения суммы чисел на нечётных позициях массива

---
+++ Task36/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа для определения суммы чисел на нечётных позициях массива, который выводит числа в случайном порядке.
+
+void EnterArr(int[] arr)
+{for (int Y = 0; Y < arr.Length; Y++)
+arr[Y] = new Random().Next(1, 100);} // [1, 99]
+
+int SumUneven(int[] arr)
+{int sum = 0;
+for (int i = 1; i < arr.Length; i+=2)
+sum = sum + arr[i];
+return sum;}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int X = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[X];
+EnterArr(arr);
+Console.WriteLine($"Случайный массив: [{string.Join(", ", arr)}]");
+Console.Write($"Сумма чисел на нечётных позициях массива = {SumUneven(arr)}");
