5d012447d6cd2308b1d68674de5fc6e15f22cfa1
Andrey-GkBrns <Oran@Gutang.ru>
2023-03-04 12:08:40 +0300

    Программа (рекурсия) для нахождения суммы натуральных чисел в указанном промежутке

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,23 +1,23 @@
 ﻿// Программа (рекурсия) для нахождения суммы натуральных чисел в указанном промежутке.
 
-string f(int Z, int Y)
-{if (Z == Y)
-return $"{Y}";
-return f(Z, Y - 1) + $", {Y}";}
+string X(int Y, int Z)
+{if (Y == Z)
+return $"{Z}";
+return X(Y, Z - 1) + $", {Z}";}
 
-int Sum(int Z, int Y)
-{int res = Z;
-if (Z == Y)
+int Sum(int Y, int Z)
+{int Res = Y;
+if (Z < Y)
 return 0;
 else 
-{Z++;
-res = Z + Sum(Z, Y);
-return res;}}
+{Y++;
+Res = Y + (Sum(Y, Z) - 1);
+return Res;}}
 
 Console.Clear();
 Console.Write("Введите 1-ое число: ");
-int Y = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 2-ое число: ");
 int Z = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(f(Y, Z));
-Console.WriteLine(Sum(Y, Z));
+Console.Write("Введите 2-ое число: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(X(Z, Y));
+Console.WriteLine(Sum(Z, Y));
